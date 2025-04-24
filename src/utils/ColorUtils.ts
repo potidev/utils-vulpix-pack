2b8d48b9992c2f1f8d71a84be21e9657069480db
duduCMT <dev.eduardo.lc@gmail.com>
2025-04-23 21:39:21 -0300

    add method in color utils

--- src/utils/ColorUtils.ts
+++ src/utils/ColorUtils.ts
@@ -1,3 +1,5 @@
+export type DarkOrLightLuminosity = 'dark' | 'light';
+
 export class ColorUtils {
   static hexWithAlpha = (hex: string, alpha: number): string => {
     if (hex.startsWith('#')) {
@@ -19,5 +21,13 @@ export class ColorUtils {
     const alphaHex = Math.round(alpha * 255).toString(16).padStart(2, '0');
 
     return `#${hex}${alphaHex}`;
-}
+  }
+  static getContrastingLuminosity = (color: string): DarkOrLightLuminosity => {
+    color = color.replace('#', '');
+    const r = parseInt(color.substring(0, 2), 16);
+    const g = parseInt(color.substring(2, 4), 16);
+    const b = parseInt(color.substring(4, 6), 16);
+    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+    return brightness > 127 ? 'dark' : 'light';
+  }
 }
